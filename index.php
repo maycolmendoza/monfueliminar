1fb8381ca8b97f04caf59a80ccb35fb4e625fb85
maycolmendoza <105061519+maycolmendoza@users.noreply.github.com>
2022-07-12 16:16:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,6 @@
    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
 <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
-<body data-fade-in="true" role="main">
     <div id="preloader" class="preloader-container">
         <div class="animation">
             <div class="player">
